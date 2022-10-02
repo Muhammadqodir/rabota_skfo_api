1601b99889d435cf69ed67dc72306655c2b1f63a
Muhammadqodir <m.qodir777@gmail.com>
2022-10-02 10:55:06 +0300

    /statistics

--- rabota-skfo/app/Http/Controllers/ApiController.php
+++ rabota-skfo/app/Http/Controllers/ApiController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Resume;
+use App\Models\Student;
 use Illuminate\Http\Request;
 use App\Models\University;
+use App\Models\Vacancy;
 
 class ApiController extends Controller
 {
+
+	public function getStat(){
+		return json_encode(
+				[
+					"status" => 'ok',
+					"total_vacancies" => Vacancy::count(),
+					"total_resumes" => Resume::count(),
+					"total_students" => Student::count(),
+					"total_universities" => University::count()
+				]
+			);
+	}
+
 	public function getUnivers(){
 		return University::all();
 	}
+
 }
